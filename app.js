23f9b42795d8e3275bb9a50daaf7cd319b672ecf
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-03-26 08:25:14 +0100

    mysql connection works

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var mysql = require('mysql');
+var mysqlconfig = require('./mysqlconfig');
+
+var db = mysql.createConnection(mysqlconfig.config);
 
 var app = express();
 
 app.get('/', function(req,res){
 	res.send('Hello World');
+});
+
+db.connect(function(err){
+	if (!err) {
+		console.log("connected to database!");
+	} else {
+		console.log("connection to database failed!");
+	}
 });
 
 app.listen(8080);
