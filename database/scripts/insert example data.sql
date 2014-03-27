928cdd3da9e37f35a8e63c6ad635a5bcced798e2
Sascha Peukert <sascha.peukert@gmail.com>
2014-03-27 17:04:58 +0100

    deleted useless attributes in kd_person and kd_person_history and fixed the insert example data-script to work with the new datamodel

--- database/scripts/insert example data.sql
+++ database/scripts/insert example data.sql
@@ -9,27 +9,76 @@ VALUES ('The Church of the Silence', 'info@silence-must-fall.org','0000...','in 
 
 -- Company-History
 INSERT INTO kolledata.kd_company_history (comh_company_id, comh_name, comh_email, comh_phone, comh_country, comh_city, comh_postcode, comh_adress, comh_url, comh_memo, comh_timestamp)
-VALUES (2,'The Church of the Papal Mainframe', 'info@mainframe.org','0000...','in Space','No','???','somewhere in the universe','http://tardis.wikia.com/wiki/Papal_Mainframe','',NOW());
+VALUES (2,'The Church of the Papal Mainframe', 'info@mainframe.org','0000...','in Space','No','???','somewhere in the universe','http://tardis.wikia.com/wiki/Papal_Mainframe','',NOW() - INTERVAL 1 DAY);
 
 -- Person
-INSERT INTO kolledata.kd_person (per_name, per_firstname, per_email_1, per_email_2, per_phone_1, per_phone_2, per_url, per_memo, per_timestamp, per_company)   
-VALUES ('Williams','Amy','amy@pond.co.uk','amy@tardis.org','01111...','0123...','http://www.bbc.co.uk/programmes/p00wqr12/profiles/amy-pond','Companion of the 11th doctor',NOW(),1);	
+INSERT INTO kolledata.kd_person (per_name, per_firstname, per_url, per_memo, per_timestamp, per_company)   
+VALUES ('Williams','Amy','http://www.bbc.co.uk/programmes/p00wqr12/profiles/amy-pond','Companion of the 11th doctor',NOW(),1);	
 
-INSERT INTO kolledata.kd_person (per_name, per_firstname, per_email_1, per_email_2, per_phone_1, per_phone_2, per_url, per_memo, per_timestamp, per_company)   
-VALUES ('Williams','Rory','rory@pond.co.uk','rory@tardis.org','01211...','0143...','http://www.bbc.co.uk/programmes/p00wqr12/profiles/rory-williams','The last centurion',NOW(),1);
+INSERT INTO kolledata.kd_person (per_name, per_firstname, per_url, per_memo, per_timestamp, per_company)   
+VALUES ('Williams','Rory','http://www.bbc.co.uk/programmes/p00wqr12/profiles/rory-williams','The last centurion',NOW(),1);
 
-INSERT INTO kolledata.kd_person (per_name, per_firstname, per_email_1, per_email_2, per_phone_1, per_phone_2, per_url, per_memo, per_timestamp, per_company)   
-VALUES ('Lem','Tasha','MotherSuperious@mainframe.org','tasha.Lem@tardis.org','00001...','00100...','http://tardis.wikia.com/wiki/Tasha_Lem','the Mother Superious of the Papal Mainframe',NOW(),2);
+INSERT INTO kolledata.kd_person (per_name, per_firstname, per_url, per_memo, per_timestamp, per_company)   
+VALUES ('Lem','Tasha','http://tardis.wikia.com/wiki/Tasha_Lem','the Mother Superious of the Papal Mainframe',NOW(),2);
 
-INSERT INTO kolledata.kd_person (per_name, per_firstname, per_email_1, per_email_2, per_phone_1, per_phone_2, per_url, per_memo, per_timestamp, per_company)   
-VALUES ('-','Strax','strax@sontaran-empire.com','strax@declare-war-on-the-moon.com','0999...','08888...','http://www.bbc.co.uk/programmes/b01rryyj/profiles/strax','The nurse',NOW(),1);
+INSERT INTO kolledata.kd_person (per_name, per_firstname, per_url, per_memo, per_timestamp, per_company)   
+VALUES ('-','Strax','http://www.bbc.co.uk/programmes/b01rryyj/profiles/strax','The nurse',NOW(),1);
 
-INSERT INTO kolledata.kd_person (per_name, per_firstname, per_email_1, per_email_2, per_phone_1, per_phone_2, per_url, per_memo, per_timestamp, per_company)   
-VALUES ('Oswald','Clara','clara@tardis.org','clara@oswald.com','0982...','04898...','http://tardis.wikia.com/wiki/Clara_Oswald','Companion of all doctors',NOW(),1);
+INSERT INTO kolledata.kd_person (per_name, per_firstname, per_url, per_memo, per_timestamp, per_company)   
+VALUES ('Oswald','Clara','http://tardis.wikia.com/wiki/Clara_Oswald','Companion of all doctors',NOW(),1);
 
 -- Person-History
-INSERT INTO kolledata.kd_person_history (perh_person_id, perh_name, perh_firstname, perh_email_1, perh_email_2, perh_phone_1, perh_phone_2, perh_url, perh_memo, perh_timestamp, perh_company)   
-VALUES (2,'Williams','Rory','rory@pond.co.uk','rory@tardis.org','01211...','0143...','http://www.bbc.co.uk/programmes/p00wqr12/profiles/rory-williams','Companion of the 11th doctor',NOW(),1);
+INSERT INTO kolledata.kd_person_history (perh_person_id, perh_name, perh_firstname, perh_url, perh_memo, perh_timestamp, perh_company)   
+VALUES (2,'Williams','Rory','http://www.bbc.co.uk/programmes/p00wqr12/profiles/rory-williams','Companion of the 11th doctor',NOW() - INTERVAL 1 DAY,1);
 
-INSERT INTO kolledata.kd_person_history (perh_person_id, perh_name, perh_firstname, perh_email_1, perh_email_2, perh_phone_1, perh_phone_2, perh_url, perh_memo, perh_timestamp, perh_company)   
-VALUES (1,'Pond','Amy','amy@pond.co.uk','amy@tardis.org','01111...','0123...','http://www.bbc.co.uk/programmes/p00wqr12/profiles/amy-pond','Companion of the 11th doctor',NOW(),1);
+INSERT INTO kolledata.kd_person_history (perh_person_id, perh_name, perh_firstname, perh_url, perh_memo, perh_timestamp, perh_company)   
+VALUES (1,'Pond','Amy','http://www.bbc.co.uk/programmes/p00wqr12/profiles/amy-pond','Companion of the 11th doctor',NOW() - INTERVAL 1 DAY,1);
+
+-- Email
+INSERT INTO kolledata.kd_email (em_person_id, em_email, em_timestamp)
+VALUES (1,'amy@pond.co.uk',NOW());
+
+INSERT INTO kolledata.kd_email (em_person_id, em_email, em_timestamp)
+VALUES (1,'amy@tardis.org',NOW());
+
+INSERT INTO kolledata.kd_email (em_person_id, em_email, em_timestamp)
+VALUES (2,'rory@tardis.org',NOW());
+
+INSERT INTO kolledata.kd_email (em_person_id, em_email, em_timestamp)
+VALUES (3,'MotherSuperious@mainframe.org',NOW());
+
+INSERT INTO kolledata.kd_email (em_person_id, em_email, em_timestamp)
+VALUES (4,'strax@sontaran-empire.com',NOW());
+
+INSERT INTO kolledata.kd_email (em_person_id, em_email, em_timestamp)
+VALUES (4,'strax@declare-war-on-the-moon.com',NOW());
+
+INSERT INTO kolledata.kd_email (em_person_id, em_email, em_timestamp)
+VALUES (5,'clara@tardis.org',NOW());
+
+-- Email-History
+INSERT INTO kolledata.kd_email_history (emh_email_id, emh_email, emh_timestamp)
+VALUES (1,'Raggedy_Man_is_real@pond.co.uk',NOW() - INTERVAL 1 DAY);
+
+-- Phone
+INSERT INTO kolledata.kd_phone (ph_person_id, ph_phone, ph_timestamp)
+VALUES (1,'01111...',NOW());
+
+INSERT INTO kolledata.kd_phone (ph_person_id, ph_phone, ph_timestamp)
+VALUES (1,'01234...',NOW());
+
+INSERT INTO kolledata.kd_phone (ph_person_id, ph_phone, ph_timestamp)
+VALUES (2,'05111234...',NOW());
+
+INSERT INTO kolledata.kd_phone (ph_person_id, ph_phone, ph_timestamp)
+VALUES (3,'00001...',NOW());
+
+INSERT INTO kolledata.kd_phone (ph_person_id, ph_phone, ph_timestamp)
+VALUES (4,'09999...',NOW());
+
+INSERT INTO kolledata.kd_phone (ph_person_id, ph_phone, ph_timestamp)
+VALUES (5,'09876...',NOW());
+
+-- Phone-History
+INSERT INTO kolledata.kd_phone_history (phh_phone_id, phh_phone, phh_timestamp)
+VALUES (1,'0444444...old',NOW() - INTERVAL 1 DAY);
