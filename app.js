3a98a1bdc1f3b044da1fdf9f7d7d78772599f75b
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-03-30 01:45:41 +0100

    empty bootstrapped jade powered layout (thanks hackathon-starter)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mysql = require('mysql');
 var mysqlconfig = require('./mysqlconfig');
 var path = require('path');
+var connectAssets = require('connect-assets');
 
 /**
  * Create MySQL Server with config data
@@ -19,9 +20,13 @@ var app = express();
  * Express configuration
  */
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(connectAssets({
+  paths: ['public/css', 'public/js'],
+  helperContext: app.locals
+}));
 
 /**
  * Load controllers.
@@ -33,27 +38,32 @@ var homeController = require('./controllers/home');
  * Routes
  */
 
- app.get('/', homeController.index);
+app.get('/', homeController.index);
+
+/**
+ * Run server
+ */
 
 db.connect(function(err){
 	if (err) {
 		console.error('✗ MySQL Connection Error. Please make sure MySQL server is running.');
 	} else {
-		console.log("connected to database!");
+		console.log("✔ Successfully connected to MySQL database.");
 	}
 });
 
-db.query('USE kolledata');
+// db.query('USE kolledata');
 
-db.query('SELECT * FROM `kd_person`', function(err, rows, fields) {
-  if (err) throw err;
+// db.query('SELECT * FROM `kd_person`', function(err, rows, fields) {
+//   if (err) throw err;
+//   console.log("\nJust some sample output from the database:")
+//   for (var i = 0; i < rows.length; i++) {
+//   	console.log(rows[i].per_firstname + " " + rows[i].per_name);
+//   };
+// });
 
-  for (var i = 0; i < rows.length; i++) {
-  	console.log(rows[i].per_firstname + " " + rows[i].per_name);
-  };
+// db.end();
+
+app.listen(app.get('port'), function() {
+  console.log("✔ Express server listening on http://localhost:%d", app.get('port'));
 });
-
-db.end();
-
-app.listen(8080);
-console.log("Listening on http://localhost:8080");
