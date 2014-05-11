ffe6458a8850dedaec2ca059cc53f0f3daba8ab6
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-05-12 01:12:03 +0200

    added alerts for edits, but bootstrap fades leave empty space... :cry:

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $("#newentrybtn").click(function(){
+    $('#newentrybtn').click(function(){
         // TODO: seriously?
         var newRow = '<tr><td><input type="text" class="form-control" id="inputDefault"></td><td></td><td></td><td></td><td></td><td></td></tr>';
         $('#datatable tr:last').after(newRow);
