2b78650c0add834e785cddecd7a8d0d37a561d8b
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-05-12 14:04:30 +0200

    memos are no longer displayed directly in a tablerow, but onclick below

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 
+    // hide all memo tablerows right from the start
+    // this is definitely *not* a good way of doing this, but the only way that seems to be working for now...
+    $('.memo').hide();
+
+    // toggle display of memo rows onClick of inforows
+    $('.inforow').click(function(){
+        $(this).next().slideToggle(200);
+    });
+
 });
