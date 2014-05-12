a007a06205ae7b0611c3159676cd6cdaf9b65e10
Sascha Peukert <sascha.peukert@gmail.com>
2014-05-12 13:35:01 +0200

    added experimental query to export tables to csv. can be usefull later

---
+++ database/scripts/export.sql
@@ -0,0 +1,8 @@
+﻿-- Experimental Query (!) to export a table as csv-file
+-- works (currently on windows but path can be changed) but 'no value' in a field is represented as "\N" which has to be manually replaced afterwards with "" to get a clean output 
+
+SELECT * INTO OUTFILE 'C:\\result.csv'
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '\\'
+LINES TERMINATED BY '\n'
+FROM kolledata.kd_company;
