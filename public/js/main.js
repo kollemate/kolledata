25c27359a527834791a7175ffec54ccf42ced9ae
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-05-12 10:45:36 +0200

    modal has a form for new and edited input

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
 
-    $('#newentrybtn').click(function(){
-        // TODO: seriously?
-        var newRow = '<tr><td><input type="text" class="form-control" id="inputDefault"></td><td></td><td></td><td></td><td></td><td></td></tr>';
-        $('#datatable tr:last').after(newRow);
-    });
-
 });
