84725de124cdb197cf38058c858dd3e2f5dcba66
Philipp Heisig <github@pheisig.de>
2014-05-19 20:29:43 +0200

    whitespace fix, sorry

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
-    $(document).ready(function() {
-
+$(document).ready(function() {
     // hide all memo tablerows right from the start
     // this is definitely *not* a good way of doing this, but the only way that seems to be working for now...
     $('.memo').hide();
