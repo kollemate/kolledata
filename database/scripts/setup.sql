ece98cdc4a86177426177df8bfb06e1e948ebd78
Sascha Peukert <sascha.peukert@gmail.com>
2014-05-28 14:18:16 +0200

    added timestamp to ba-tables; changed setup-script to fit the new schema. added history-trigger for the new bank_accounts table

--- database/scripts/setup.sql
+++ database/scripts/setup.sql
@@ -13,6 +13,7 @@ CREATE TABLE kolledata.kd_company (
   com_email2 VARCHAR(100),
   com_phone1 VARCHAR(20),
   com_phone2 VARCHAR(20),
+  com_fax VARCHAR(20),
   com_country VARCHAR(100),
   com_city VARCHAR(100),
   com_postcode VARCHAR(10),
@@ -32,6 +33,7 @@ CREATE TABLE kolledata.kd_company_history (
   comh_email2 VARCHAR(100),
   comh_phone1 VARCHAR(20),
   comh_phone2 VARCHAR(20),
+  com_fax VARCHAR(20),
   comh_country VARCHAR(100),
   comh_city VARCHAR(100),
   comh_postcode VARCHAR(10),
@@ -46,12 +48,17 @@ CREATE TABLE kolledata.kd_company_history (
 
 CREATE TABLE kolledata.kd_person (
   per_id INT NOT NULL AUTO_INCREMENT,
+  per_salutation VARCHAR(50),
+  per_academic_title VARCHAR(50),
   per_name VARCHAR(200),
   per_firstname VARCHAR(200),
+  per_fax VARCHAR(50),
   per_url VARCHAR(2083),
   per_memo LONGTEXT,
   per_timestamp DATETIME,
   per_company INT,
+  per_department VARCHAR(100),
+  per_arbitrator INT,
   PRIMARY KEY (per_id)
   -- FOREIGN KEY (per_company) REFERENCES kd_company(com_id)
   -- ON DELETE CASCADE ON UPDATE CASCADE
@@ -60,12 +67,17 @@ CREATE TABLE kolledata.kd_person (
 CREATE TABLE kolledata.kd_person_history (
   perh_id INT NOT NULL AUTO_INCREMENT,
   perh_person_id INT,
+  perh_salutation VARCHAR(50),
+  perh_academic_title VARCHAR(50),
   perh_name VARCHAR(200),
   perh_firstname VARCHAR(200),
+  perh_fax VARCHAR(50),
   perh_url VARCHAR(2083),
   perh_memo LONGTEXT,
   perh_timestamp DATETIME,
   perh_company INT,
+  perh_department VARCHAR(100),
+  perh_arbitrator INT,
   PRIMARY KEY (perh_id)
   -- FOREIGN KEY (perh_person_id) REFERENCES kd_person(per_id)
   -- ON DELETE CASCADE -- ON UPDATE CASCADE
@@ -111,6 +123,37 @@ CREATE TABLE kolledata.kd_phone_history (
   -- ON DELETE CASCADE -- ON UPDATE CASCADE
 );
 
+CREATE TABLE kolledata.kd_bank_accounts (
+  ba_id INT NOT NULL AUTO_INCREMENT,
+  ba_owner_id INT,
+  ba_type_owner VARCHAR(1),
+  ba_account_number VARCHAR(20),
+  ba_bank_code VARCHAR(10),
+  ba_IBAN VARCHAR(30),
+  ba_BIC VARCHAR(11),
+  ba_bank_name VARCHAR(50),
+  ba_tax_number VARCHAR(20),
+  ba_sales_tax_ident_number VARCHAR(20),
+  ba_timestamp DATETIME,
+  PRIMARY KEY (ba_id)
+);
+
+CREATE TABLE kolledata.kd_bank_accounts_history (
+  bah_id INT NOT NULL AUTO_INCREMENT,
+  bah_bank_account_id INT,
+  bah_owner_id INT,
+  bah_type_owner VARCHAR(1),
+  bah_account_number VARCHAR(20),
+  bah_bank_code VARCHAR(10),
+  bah_IBAN VARCHAR(30),
+  bah_BIC VARCHAR(11),
+  bah_bank_name VARCHAR(50),
+  bah_tax_number VARCHAR(20),
+  bah_sales_tax_ident_number VARCHAR(20),
+  bah_timestamp DATETIME,
+  PRIMARY KEY (bah_id)
+);
+
 -- CREATING THE TRIGGER
 DELIMITER $$
 
@@ -132,6 +175,7 @@ CREATE
       comh_email2,
 			comh_phone1,
       comh_phone2,
+      comh_fax,
 			comh_country,
 			comh_city,
 			comh_postcode,
@@ -147,6 +191,7 @@ CREATE
       OLD.com_email2,
 			OLD.com_phone1,
       OLD.com_phone2,
+      OLD.com_fax,
 			OLD.com_country,
 			OLD.com_city,
 			OLD.com_postcode,
@@ -171,6 +216,7 @@ CREATE
       comh_email2,
 			comh_phone1,
       comh_phone2,
+      comh_fax,
 			comh_country,
 			comh_city,
 			comh_postcode,
@@ -186,6 +232,7 @@ CREATE
       OLD.com_email2,
 			OLD.com_phone1,
       OLD.com_phone2,
+      OLD.com_fax,
 			OLD.com_country,
 			OLD.com_city,
 			OLD.com_postcode,
@@ -210,21 +257,31 @@ CREATE
 		-- copy old stuff in history table
 		INSERT INTO kolledata.kd_person_history (
 			perh_person_id,
+      perh_salutation,
+      perh_academic_title,
       perh_name,
       perh_firstname,
+      perh_fax,
       perh_url,
       perh_memo,
       perh_timestamp,
-      perh_company
+      perh_company,
+      perh_department,
+      perh_arbitrator
 		) 
 		VALUES (
-			NEW.per_id, 
+			NEW.per_id,
+      OLD.per_salutation,
+      OLD.per_academic_title,
 			OLD.per_name,
       OLD.per_firstname,
+      OLD.per_fax,
       OLD.per_url,
       OLD.per_memo,
       OLD.per_timestamp,
-      OLD.per_company
+      OLD.per_company,
+      OLD.per_department,
+      OLD.per_arbitrator
 		);
 
 END$$
@@ -236,21 +293,31 @@ CREATE
 		-- copy old stuff in history table
 		INSERT INTO kolledata.kd_person_history (
 			perh_person_id,
+      perh_salutation,
+      perh_academic_title,
       perh_name,
       perh_firstname,
+      perh_fax,
       perh_url,
       perh_memo,
       perh_timestamp,
-      perh_company
+      perh_company,
+      perh_department,
+      perh_arbitrator
 		) 
 		VALUES (
 			OLD.per_id, 
+			OLD.per_salutation,
+      OLD.per_academic_title,
 			OLD.per_name,
       OLD.per_firstname,
+      OLD.per_fax,
       OLD.per_url,
       OLD.per_memo,
       OLD.per_timestamp,
-      OLD.per_company
+      OLD.per_company,
+      OLD.per_department,
+      OLD.per_arbitrator
 		);
 
 END$$
@@ -336,6 +403,84 @@ CREATE
 			OLD.ph_id, 
 			OLD.ph_phone,
       OLD.ph_timestamp
+		);
+
+END$$
+
+
+CREATE
+	TRIGGER kolledata.bank_accounts_history_trigger AFTER UPDATE 
+	ON kolledata.kd_bank_accounts 
+	FOR EACH ROW BEGIN
+		
+		-- update old references if present
+		UPDATE kolledata.kd_bank_accounts_history 
+          SET bah_bank_account_id= NEW.ba_id
+        WHERE bah_bank_account_id=OLD.ba_id;
+
+		-- copy old stuff in history table
+		INSERT INTO kolledata.kd_bank_accounts_history (
+			bah_id,
+      bah_bank_account_id,
+      bah_owner_id,
+      bah_type_owner,
+      bah_account_number,
+      bah_bank_code,
+      bah_IBAN,
+      bah_BIC,
+      bah_bank_name,
+      bah_tax_number,
+      bah_sales_tax_ident_number,
+      bah_timestamp
+		) 
+		VALUES (
+			NEW.ba_id, 
+			OLD.ba_owner_id,
+      OLD.ba_type_owner,
+      OLD.ba_account_number,
+      OLD.ba_bank_code,
+      OLD.ba_IBAN,
+      OLD.ba_BIC,
+      OLD.ba_bank_name,
+      OLD.ba_tax_number,
+      OLD.ba_sales_tax_ident_number,
+      OLD.ba_timestamp
+		);
+
+END$$
+
+CREATE
+	TRIGGER kolledata.bank_accounts_delete_trigger AFTER DELETE 
+	ON kolledata.kd_bank_accounts 
+	FOR EACH ROW BEGIN
+		
+		-- copy old stuff in history table
+		INSERT INTO kolledata.kd_bank_accounts_history (
+			bah_id,
+      bah_bank_account_id,
+      bah_owner_id,
+      bah_type_owner,
+      bah_account_number,
+      bah_bank_code,
+      bah_IBAN,
+      bah_BIC,
+      bah_bank_name,
+      bah_tax_number,
+      bah_sales_tax_ident_number,
+      bah_timestamp
+		) 
+		VALUES (
+			OLD.ba_id, 
+			OLD.ba_owner_id,
+      OLD.ba_type_owner,
+      OLD.ba_account_number,
+      OLD.ba_bank_code,
+      OLD.ba_IBAN,
+      OLD.ba_BIC,
+      OLD.ba_bank_name,
+      OLD.ba_tax_number,
+      OLD.ba_sales_tax_ident_number,
+      OLD.ba_timestamp
 		);
 
 END$$
