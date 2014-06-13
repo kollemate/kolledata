1c0323236133e7be2fe42cb182259ba68748e3e4
Gilbert Röhrbein <payload@payload-bay.de>
2014-06-13 18:09:43 +0200

    refactor addPerson: ripped apart into smaller functions, used async.js

--- controllers/persons.js
+++ controllers/persons.js
@@ -1,5 +1,8 @@
 // this is needed for mysql.format prepared statements, not db connections
 var mysql = require('mysql');
+var async = require('async');
+
+
 
 module.exports = function(db) {
     // this format is adapted from here:
@@ -27,63 +30,88 @@ module.exports = function(db) {
         });
     };
 
-    module.addPerson = function(req, res) {
-        var firstName = req.body.firstName;
-        var lastName = req.body.lastName;
-        var url = req.body.url;
-        var email = req.body.email;
-        var memo = req.body.memo;
-        var company = req.body.company;
 
-        // get company id of chosen company from add new person dropdown
-        var sql = 'SELECT com_id FROM kd_company WHERE com_name = ?;';
-        var inserts = [company];
-        sql = mysql.format(sql, inserts);
-        db.query(sql, function(err,rows,fields){
-            if (company === '- N/A -') {
-                company = null;
-            } else {
-                company = rows[0].com_id;
-            }
 
-            // insert person with the now correct company into the database
-            var sql = 'INSERT INTO kd_person (per_name, per_firstname, per_url, per_memo, per_timestamp, per_company) VALUES (?, ?, ?, ?, NOW(), ?)';
-            var inserts = [lastName, firstName, url, memo, company];
-            sql = mysql.format(sql, inserts);
-            db.query(sql, function(err, rows){
-                if (err) throw err;
-
-                // add email to the just changed row (rows.insertId is the id of the just added row)
-                var sql = 'INSERT INTO kolledata.kd_email (em_person_id, em_email, em_timestamp) VALUES (?, ?, NOW());';
-                var inserts = [rows.insertId, email];
-                sql = mysql.format(sql, inserts);
-                db.query(sql, function(err){
-                    if (err) throw err;
-
-                    // get all persons joined with their respective company names
-                    var sql = 'SELECT * FROM kd_person LEFT OUTER JOIN kd_company ON kd_person.per_company = kd_company.com_id;';
-                    db.query(sql, function(err, rows, fields) {
-                        if (err) throw err;
-                        var persons = rows;
-
-                        // get all companies to pass on to jade so that the dropdown
-                        // in the add persons modal can be displayed
-                        var sql = 'SELECT * FROM kd_company;';
-                        db.query(sql, function(err, rows, fields){
-                            if (err) throw err;
-                            var companies = rows;
-
-                            res.render('persons', {
-                                title: 'Persons',
-                                results: persons,
-                                companies: companies
-                            });
-                        });
-                    });
-                });
+    function addPerson(req, res) {
+        var person = req.body;
+        async.waterfall([
+            async.apply(getCompanyIdByName, person.company),
+            async.apply(insertPerson, person),
+            async.apply(insertEmail, person)],
+        function (err) {
+            // TODO inserting a person failed
+        });
+        async.parallel({
+            persons     : getPersonsWithCompanies,
+            companies   : getCompanies
+        }, function (err, data) {
+            // TODO getting persons or companies failed
+            res.render('persons', {
+                title       : 'Persons',
+                results     : data.persons,
+                companies   : data.companies
             });
         });
     };
+
+    module.addPerson = addPerson;
+
+    function getCompanyIdByName(com_name, cb) {
+        // get company id of chosen company from add new person dropdown
+        var sql     = 'SELECT com_id FROM kd_company \
+                       WHERE com_name = ?;';
+        var inserts = [com_name];
+        var query   = mysql.format(sql, inserts);
+        db.query(sql, function(err, rows) {
+            if (err) return cb(err);
+            if (com_name === '- N/A -') {
+                cb(null, null);
+            } else {
+                cb(null, rows[0].com_id);
+            }
+        });
+    }
+
+    function insertPerson(person, com_id, cb) {
+        // insert person with the now correct company into the database
+        var sql     = 'INSERT INTO kd_person        \
+            (per_name, per_firstname, per_url,      \
+             per_memo, per_timestamp, per_company)  \
+            VALUES (?, ?, ?, ?, NOW(), ?)';
+        var p       = person;
+        var inserts = [p.lastName, p.firstName, p.url, p.memo, com_id];
+        var query   = mysql.format(sql, inserts);
+        db.query(sql, function(err, rows) {
+            cb(err, rows.insertId);
+        });
+    }
+
+    function insertEmail(person, insertId, cb) {
+        // add email to the just changed row (rows.insertId is the id of the just added row)
+        var sql     = 'INSERT INTO kolledata.kd_email   \
+            (em_person_id, em_email, em_timestamp)      \
+            VALUES (?, ?, NOW());';
+        var inserts = [insertId, person.email];
+        var query   = mysql.format(sql, inserts);
+        db.query(sql, cb);
+    }
+
+    function getPersonsWithCompanies(cb) {
+        // get all persons joined with their respective company names
+        var sql     = 'SELECT * FROM kd_person  \
+            LEFT OUTER JOIN kd_company          \
+            ON kd_person.per_company = kd_company.com_id;';
+        db.query(sql, function(err, rows) {
+            var persons = rows;
+            cb(err, persons);
+        });
+    }
+
+    function getCompanies(cb) {
+        db.query('SELECT * FROM kd_company;', cb);
+    }
+
+
 
     module.removePerson = function(req, res) {
         var id = req.body.id;
