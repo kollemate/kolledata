d183963fec2a958133332787db2b4a929af0f9ae
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-06-14 02:22:35 +0200

    single person page now displays a bit more info

--- controllers/persons.js
+++ controllers/persons.js
@@ -30,7 +30,7 @@ module.exports = function(db) {
     module.getPerson = function(req, res) {
         var per_id = req.params.id;
 
-        var sql = 'SELECT * FROM kd_person WHERE per_id = ?';
+        var sql = 'SELECT * FROM kd_person LEFT OUTER JOIN kd_company ON kd_person.per_company = kd_company.com_id WHERE per_id = ?';
         var inserts = [per_id];
         sql = mysql.format(sql, inserts);
         db.query(sql, function(err, rows, fields){
@@ -38,9 +38,22 @@ module.exports = function(db) {
 
             var person = rows;
 
-            res.render('person', {
-                title: rows[0]['per_firstname'] + ' ' + rows[0]['per_name'],
-                results: person
+            var sql = 'SELECT em_email FROM kolledata.kd_email WHERE em_person_id = ?';
+            var inserts = [per_id];
+            sql = mysql.format(sql, inserts);
+            db.query(sql, function(err, rows, fields){
+                if (err) throw err;
+
+                var emails = rows;
+
+                res.render('person', {
+                    title: person[0]['per_firstname'] + ' ' + person[0]['per_name'],
+                    results: person,
+                    emails: emails
+                });
+
+                // res.set('Content-Type', 'text/json');
+                // res.send(person);
             });
         });
     };
