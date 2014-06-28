cd66380e0166b1924c24a9627fb2a3afdb672b65
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-06-28 12:38:00 +0200

    small typo in the demo db commands

--- database/scripts/getData.sql
+++ database/scripts/getData.sql
@@ -1,4 +1,4 @@
-﻿-- No real scripts (!), just examples showing what you must do to get specific data. 
+﻿-- No real scripts (!), just examples showing what you must do to get specific data.
 
 -- Some simple Select-Statements (not all)
 -- Get all Company-Data (rather don't display the com_id to the user)
@@ -12,19 +12,19 @@ WHERE  com_id= 1 -- Instead of 1, insert the ID of the company whose data you wa
 ;
 
 -- Get all data that belongs to a specific person
-SELECT * 
-FROM kolledata.kd_company
+SELECT *
+FROM kolledata.kd_person
 WHERE per_id =1 -- Instead of 1, insert the ID of the person whose data you want to select
 ;
 
 -- Get all the Emailaddresses of a (given) person
-SELECT em_email 
+SELECT em_email
 FROM kolledata.kd_email
 WHERE em_person_id =1 -- Instead of 1, insert the ID of the person whose emailaddresses you want to select
 ;
 
 -- Get all the Phonenumbers of a (given) person
-SELECT ph_phone 
+SELECT ph_phone
 FROM kolledata.kd_phone
 WHERE ph_person_id =1 -- Instead of 1, insert the ID of the person whose phonenumbers you want to select
 ;
@@ -43,18 +43,18 @@ WHERE per_id = 1 -- Instead of 1, insert the ID of the person whose data you wan
 ;
 
 -- Get the most current timestamp that a specific person, their email or their phone was changed (e.g. to be displayed to the user)
-SELECT 
-	CASE 
-		WHEN Helper.P>=Helper.E and Helper.P>=Helper.PH THEN 
-		Helper.P 
-		WHEN Helper.E>=Helper.P and Helper.E>=Helper.PH THEN 
-		Helper.E 
-		WHEN Helper.PH>=Helper.P and Helper.PH>=Helper.E THEN 
-		Helper.E
-	END as most_current_modified_date
+SELECT
+    CASE
+        WHEN Helper.P>=Helper.E and Helper.P>=Helper.PH THEN
+        Helper.P
+        WHEN Helper.E>=Helper.P and Helper.E>=Helper.PH THEN
+        Helper.E
+        WHEN Helper.PH>=Helper.P and Helper.PH>=Helper.E THEN
+        Helper.E
+    END as most_current_modified_date
 FROM (
-	SELECT distinct per_timestamp AS P, em_timestamp AS E, ph_timestamp AS PH FROM kolledata.kd_person, kolledata.kd_email, kolledata.kd_phone
-	WHERE per_id=1 -- Instead of 1, insert the ID of the person whose data you want to select
+    SELECT distinct per_timestamp AS P, em_timestamp AS E, ph_timestamp AS PH FROM kolledata.kd_person, kolledata.kd_email, kolledata.kd_phone
+    WHERE per_id=1 -- Instead of 1, insert the ID of the person whose data you want to select
     and em_person_id=per_id and ph_person_id=per_id) AS helper
 ;
 
