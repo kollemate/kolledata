83e2b57f3d63d146ce279e2f615e4b46f0b5b1c5
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-06-30 13:42:14 +0200

    colors are better than alerts

--- public/js/validations.js
+++ public/js/validations.js
@@ -7,17 +7,21 @@ function validateURL() {
     };
     if (!validator.isURL(document.getElementById('url').value, options)) {
         document.getElementById('submitbtn').classList.add('disabled');
-        alert('Please enter a valid URL including http://.');
+        document.getElementById('urlgroup').classList.add('has-error');
     } else {
         document.getElementById('submitbtn').classList.remove("disabled");
+        document.getElementById('urlgroup').classList.remove('has-error');
+        document.getElementById('urlgroup').classList.add('has-success');
     }
 }
 
 function validateEmail() {
     if (!validator.isEmail(document.getElementById('email').value)) {
         document.getElementById('submitbtn').classList.add('disabled');
-        alert('Please enter a valid e-mail address.');
+        document.getElementById('emailgroup').classList.add('has-error');
     } else {
         document.getElementById('submitbtn').classList.remove("disabled");
+        document.getElementById('emailgroup').classList.remove('has-error');
+        document.getElementById('emailgroup').classList.add('has-success');
     }
 }
