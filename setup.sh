b82184d208a3dbe6e95e30d9c70ff9cb33eeffc6
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-07-08 02:36:29 +0200

    a setup script would be nice, here's a start

---
+++ setup.sh
@@ -0,0 +1,52 @@
+#! /bin/sh
+
+# this script will perform basic setup and get you ready to start the app
+# node & mysql should already be installed and running
+
+# TODO: how could we make this work with travis? maybe prefill the variables/default fallback?
+echo "What's your MySQL host? (Probably localhost)"
+read -e HOST
+
+echo "What's your MySQL user? (Probably root)"
+read -e USERNAME
+
+echo "That user's password for accessing the database? (Probably root as well)"
+read -e PASSWORD
+
+echo "What's your MySQL port? (default is 8889)"
+read -e PORT
+
+
+
+# 1. install npm and bower dependencies
+
+echo "1. Installing necessary dependencies through npm and bower"
+npm install
+bower install
+
+
+
+# 2. create mysql config
+
+echo "2. Creating MySQL config file"
+cp -n example_mysqlconfig.js mysqlconfig.js
+
+
+
+# 3. setup the databases if not there already
+
+echo "3. Setting up the databases"
+# mysql -u $USERNAME -p $PASSWORD -h $HOST < database/scripts/setup.sql
+
+while true; do
+    read -p "Do you want to pre-fill the database with some sample entries? (y/n)" yn
+    case $yn in
+        [Yy]* ) mysql -u $USERNAME -p $PASSWORD -h $HOST < database/scripts/insertExampleData.sql; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+
+echo "You're all set, run \`node app.js\` to start the application"
